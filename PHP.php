c5e9381f3c3cf607f83763ac6c869f54c64b0e1e
buncho315 <mzh.hrtk125@gmail.com>
2022-10-12 06:30:22 +0000

    else

--- PHP.php
+++ PHP.php
@@ -7,5 +7,29 @@
     $greeting = "hello world";
     echo $greeting;
 //変数
+  $name = "paiza";
+    echo $name. "さん、冒険へ行こう！"
+    //変数と文字列の連結
 ?>
 
+<?php
+    $name = trim(fgets(STDIN));
+    echo "Hello " . $name;
+    //fgets関数で入力タブから受け取ったデータを表示する
+    $number = trim(fgets(STDIN));
+    echo $number * 10 . "\n";
+    echo "お小遣い：" . ($number * 10) . "円";
+?>
+<!--//else-->
+<?php
+    $name = trim(fgets(STDIN));
+    echo "Hello " . $name . "\n";
+
+    if ($name == "PHP") {
+        echo "Welcome\n";
+    } elseif ($name == "PHP") {
+        echo "Good morning\n";
+    } else {
+        echo "Goodbye!\n";
+    }
+?>
